138c47b5bf821d702b1e859b12b6c659decabd60
albertojulio <47988700+albertojulio@users.noreply.github.com>
2020-05-18 20:37:44 -0300

    Update rachacuca.py

--- rachacuca.py
+++ rachacuca.py
@@ -1,26 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
-
-url = 'https://rachacuca.com.br/quiz/18992/bleach-sagas/'
-
+ 
+url = 'https://rachacuca.com.br/quiz/solve/77750/one-piece-akuma-no-mi/'
 r = requests.get(url)
-
+ 
 soup = BeautifulSoup(r.content, 'html.parser')
-
-soup
-
-perguntas = soup.find('ol').li.p.text
-#print(perguntas)
-respostas = soup.find('ol').find('div', class_='alternativa-texto').p.text
-#print(respostas)
-
-todos_elementos = soup.find_all('ol')
-#print(todos_elementos)
-
-for elemento in todos_elementos:
-  perguntas = elemento.find('ol')li.p.text
-  respostas = elemento.find('div', class_='alternativa-texto').p.text
-
-  print(f'perguntas: {perguntas}')
-  print(f'respostas: {respostas}')
-  print('-'*70)
+resultado = soup.findAll('p')
+ 
+perguntas = {}
+respostas = []
+achou_pergunta = False
+pergunta = ''
+num_resposta = 0
+ 
+for linha in resultado:
+    if '?' in linha.get_text():
+        if achou_pergunta:
+            perguntas[pergunta] = respostas
+            respostas = []
+            num_resposta = 0
+        pergunta = linha.get_text()
+        perguntas[pergunta] = []
+        achou_pergunta = True
+    elif '?' not in linha.get_text() and achou_pergunta:
+        if num_resposta < 5:
+            respostas.append(linha.get_text())
+            num_resposta += 1
+        
+perguntas[pergunta] = respostas
+ 
+for pergunta, resposta in perguntas.items():
+    print(pergunta,':', resposta)
